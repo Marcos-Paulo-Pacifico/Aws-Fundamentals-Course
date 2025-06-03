1a1cd82990e90fbf5694a536368d775cbb4c8808
Marcos Paulo Pacifico <paulopacific.pp@gmail.com>
2025-06-02 21:35:44 -0300

    Secrets Manager

---
+++ SecretsManager.App/Program.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerClient = new AmazonSecretsManagerClient();
+
+var listSecretVersionRequest = new ListSecretVersionIdsRequest()
+{
+    SecretId = "ApiKey",
+    IncludeDeprecated = true
+};
+
+var versionResponse = await secretsManagerClient.ListSecretVersionIdsAsync(listSecretVersionRequest);
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "ApiKey"
+};
+
+var response = await secretsManagerClient.GetSecretValueAsync(request);
+Console.WriteLine(response.SecretString);
+
+var describeRequest = new DescribeSecretRequest()
+{
+    SecretId = "ApiKey"
+};
+var describeResponse = await secretsManagerClient.DescribeSecretAsync(describeRequest);
+
